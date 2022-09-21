dba8f14427985ac57f2f9c31b98e8cbe944953ec
Tomás Chaves <tomcchaves@gmail.com>
2022-09-21 18:16:20 -0300

    Adição exercícios das aulas.

---
+++ fundamentos/bloco-04-introducao-a-javascript/dia-1-javascript-primeiros-passos/variaveis.js
@@ -0,0 +1,12 @@
+const myName = 'Tomás';
+const birthCity = 'Belo Horizonte';
+let birthYear = 1995;
+
+
+birthYear = 2030;
+birthCity = 'Contagem';
+
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
