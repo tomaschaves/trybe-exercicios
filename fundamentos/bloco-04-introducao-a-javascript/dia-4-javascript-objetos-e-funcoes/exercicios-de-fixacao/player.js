d82a28e8990f49e436939aeaa6d4a954496b5940
Tomás Chaves <tomcchaves@gmail.com>
2022-09-25 21:25:23 -0300

    Adição exercícios fixação objetos.

---
+++ fundamentos/bloco-04-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercicios-de-fixacao/player.js
@@ -0,0 +1,24 @@
+//let name = 'Marta';
+//let lastName = 'Silva';
+//let age = 34;
+//let medals = { golden: 2, silver: 3 };
+
+let player = {
+    name: 'Marta',
+    lastName: 'Silva',
+    age: 34,
+    medals:{
+        golden: 1,
+        silver: 3
+    }
+}
+
+console.log(player);
+console.log('');
+console.log('Sobrenome da jogadora: ' + player.name);
+console.log('Idade da jogadora: ' + player.lastName);
+console.log('Nome da jogadora: ' + player.age);
+console.log('Medalhas de ouro da jogadora: ' + player.medals.golden);
+console.log('Medalhas de prata da jogadora: ' + player.medals.silver);
+
+
