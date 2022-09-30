6c58495114ba281d95989000df130e1fc7d027e3
Tomás Chaves <tomcchaves@gmail.com>
2022-09-30 15:12:03 -0300

    Adição exercícios dia 5.2.

---
+++ fundamentos/bloco-05-javaScript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/exerciciosFixacao/criandoElementos.js
@@ -0,0 +1,15 @@
+
+let irmao = ;
+
+
+let filho = ;
+
+//filhoFilhoDoFilho
+let bisneto = ; 
+
+/*
+Crie um irmão para elementoOndeVoceEsta.
+Crie um filho para elementoOndeVoceEsta.
+Crie um filho para primeiroFilhoDoFilho.
+A partir desse filho criado, acesse terceiroFilho.
+*/
