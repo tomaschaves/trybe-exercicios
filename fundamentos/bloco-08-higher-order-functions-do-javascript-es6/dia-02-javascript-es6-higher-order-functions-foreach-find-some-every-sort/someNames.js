18ae886897d01138493f55d3d2b5cf0ba1f4c252
Tomás Chaves <tomcchaves@gmail.com>
2022-10-25 16:14:21 -0300

    Adição exercícios some.

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/someNames.js
@@ -0,0 +1,7 @@
+const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+
+const hasName = (arr, name) => {
+  return arr.some((currentName) => currentName === name);
+};
+
+console.log(hasName(names, 'Ana'));
