a05824785bc53e3e6241344182acac9921bc0682
Tomás Chaves <tomcchaves@gmail.com>
2022-10-26 20:22:39 -0300

    Adição exercício moreThan60Years.

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-map-e-filter/moreThan60Years.js
@@ -0,0 +1,31 @@
+const library = require('./data');
+const expectedResult = [
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+    releaseYear: 1928,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: { name: 'Isaac Asimov', birthYear: 1920 },
+    releaseYear: 1951,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954,
+  },
+];
+
+function oldBooksOrdered() {
+  const filtered = library.filter((book) => ((2022 - book.releaseYear) > 60))
+  .sort((a, b) => a.releaseYear - b.releaseYear);
+  return filtered;
+}
+console.log(oldBooksOrdered());
