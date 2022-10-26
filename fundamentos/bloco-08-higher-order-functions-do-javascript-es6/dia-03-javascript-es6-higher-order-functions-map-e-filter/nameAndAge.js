d8e22cf6a440fdb99df22a6d996c89efd3c2bb71
Tomás Chaves <tomcchaves@gmail.com>
2022-10-26 19:55:20 -0300

    Adição exercício nameAndAge.

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-map-e-filter/nameAndAge.js
@@ -0,0 +1,40 @@
+const books = require('./data');
+const library = require('./data');
+const expectedResult = [
+  {
+    age: 31,
+    author: 'Isaac Asimov',
+  },
+  {
+    age: 38,
+    author: 'H. P. Lovecraft',
+  },
+  {
+    age: 39,
+    author: 'Stephen King',
+  },
+  {
+    age: 43,
+    author: 'George R. R. Martin',
+  },
+  {
+    age: 45,
+    author: 'Frank Herbert',
+  },
+  {
+    age: 62,
+    author: 'J. R. R. Tolkien',
+  },
+];
+
+function nameAndAge() {
+  return library.map((book) => (
+    {
+      age:book.releaseYear - book.author.birthYear,
+      author:book.author.name,
+    }
+  ))
+  .sort((a, b) => a.age - b.age);
+}
+
+console.log(nameAndAge());
