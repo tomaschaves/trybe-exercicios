7ca50e690b878e703dbaba4e63827f779105123a
Tomás Chaves <tomcchaves@gmail.com>
2022-10-27 16:08:34 -0300

    Adição exercício 1.

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-higher-order-functions-reduce/exercicio1.js
@@ -0,0 +1,12 @@
+// Utilize o reduce para transformar uma matriz em um array.
+const arrays = [
+  ['1', '2', '3'],
+  [true],
+  [4, 5, 6],
+];
+
+function flatten() {
+  return arrays.reduce((acc, array) => acc.concat(array), []);
+}
+
+console.log(flatten());
