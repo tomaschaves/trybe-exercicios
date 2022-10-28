a5c4587194f272e5fea8d365d45832a3c03e6e7f
Tomás Chaves <tomcchaves@gmail.com>
2022-10-28 14:23:29 -0300

    Adição exercício de fixação spread.

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parâmetro-rest-destructuring-e-mais/exemploSpread.js
@@ -0,0 +1,11 @@
+// Faça uma lista com as suas frutas favoritas
+const specialFruit = ['Amora', 'Morango', 'Pêssego'];
+
+// Faça uma lista de complementos que você gostaria de adicionar
+const additionalItens = ['Calda de creme de leite com açúcar', 'Chocolate', 'Leite condensado'];
+
+const fruitSalad = (fruit, additional) => {
+  return newList = [...specialFruit, ...additionalItens];
+};
+
+console.log(fruitSalad(specialFruit, additionalItens));
