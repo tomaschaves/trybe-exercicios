6b18e50f9022c469b53705fe871dc45d174f6c40
Tomás Chaves <tomcchaves@gmail.com>
2022-10-28 11:06:05 -0300

    Adição exercício 3.

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-higher-order-functions-reduce/exercicio3.js
@@ -0,0 +1,25 @@
+// Dado o array de nomes, utilize o reduce para retornar a quantidade de vezes em que aparece a letra a, maiúscula ou minúscula.
+const names = [
+  'Aanemarie', 'Adervandes', 'Akifusa',
+  'Abegildo', 'Adicellia', 'Aladonata',
+  'Abeladerco', 'Adieidy', 'Alarucha',
+];
+
+const expectedResult = 20;
+
+function containsA() {
+  let numbersOfA = 0; // variável para guardar o número de vezes que a letra a aparece
+  names.forEach((name) => { //para cada uma das palavras, quantas vezes a letra a aparece em cada nome(name)?
+    const letters = name.split(''); //para transformar cada um dos nomes, que é uma string, em um array e facilitar a contagem. isso fará com que cada uma das letras se torne um elemento dentro do array
+    numbersOfA += letters.reduce((totalOfA, currentLetter) => {   //reduzir cada uma das palavras em um número, a qtdade de letras a que aparece em cada uma
+      if (currentLetter.toLowerCase() === 'a') {    //verificamos se a letra atual é igual a 'a' ou 'A' 
+        return totalOfA + 1;                        //aumentamos o reultado se sim
+      }
+      return totalOfA;                              //deixamos igual se não
+    }, 0);
+  });
+
+  return numbersOfA;
+}
+
+console.log(containsA());
