4370d1e4f3028efc5f43e67b04c5ad25c0f194b9
Tomás Chaves <tomcchaves@gmail.com>
2022-10-30 14:53:51 -0300

    Adição exercício2.js e enunciado exercício1.js.

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parâmetro-rest-destructuring-e-mais/exercicio1.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parâmetro-rest-destructuring-e-mais/exercicio1.js
@@ -1,3 +1,5 @@
+// Dado o código abaixo, altere a chamada da função rectangleArea de forma que seja impressa a área dos 3 retângulos (rectangle1, rectangle2 e rectangle3). O código deve retornar em sequência 2, 15 e 54.
+
 const rectangleArea = (width, height) => width * height;
 
 const rectangle1 = [1, 2];
