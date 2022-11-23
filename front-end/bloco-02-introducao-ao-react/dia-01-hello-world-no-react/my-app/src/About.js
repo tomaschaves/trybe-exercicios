7705fcd3fd0c3e0de6e015445dd22dc73c15beb7
Tomás Chaves <tomcchaves@gmail.com>
2022-11-22 21:10:17 -0300

    Adição exercícios de fixação.

---
+++ front-end/bloco-02-introducao-ao-react/dia-01-hello-world-no-react/my-app/src/About.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class About extends React.Component {
+ render () {
+ return (  
+ <div>
+    <h1>
+      {"Tomás"}
+    </h1>
+    <p>
+      {"Oi, eu sou o Tomás!"}
+    </p>
+    <h2>
+      {"Minhas habilidades"}
+    </h2>
+    <ul>
+      <li>{'Planilhas'}</li>
+      <li>{'Photoshop'}</li>
+      <li>{'Sketchup'}</li>
+    </ul>
+  </div>
+  );
+ }
+}
+
+export default About;
